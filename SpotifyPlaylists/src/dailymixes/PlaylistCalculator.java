eb02d16ebc1dd0ead56ae1df86b0d4dafc1b569a
Racquell-G <159744491+Racquell-G@users.noreply.github.com>
2024-08-07 14:28:31 -0400

    Update PlaylistCalculator.java

--- SpotifyPlaylists/src/dailymixes/PlaylistCalculator.java
+++ SpotifyPlaylists/src/dailymixes/PlaylistCalculator.java
@@ -1,9 +1,3 @@
-// Virginia Tech Honor Code Pledge:
-// f
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Racquell Grey racquellg
 
 package dailymixes;
 
